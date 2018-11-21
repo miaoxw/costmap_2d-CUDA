b5245e1590af2530d091da9c312527784f87ebe8
Miau Shiauwe <miau.shiauwe@smail.nju.edu.cn>
2018-11-21 17:14:31 +0800

    Stub for debug
    costmap_2d::cuda::static_layer::rollingUpdateCosts

--- src/costmap_2d/plugins/static_layer.cpp
+++ src/costmap_2d/plugins/static_layer.cpp
@@ -321,7 +321,9 @@ void StaticLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int
       ROS_ERROR("%s", ex.what());
       return;
     }
-    costmap_2d::cuda::static_layer::rollingUpdateCosts(master_grid, transform, layered_costmap_->getCostmap(), use_maximum_, min_i, min_j, max_i, max_j);
+    ROS_ERROR("rollingUpdateCosts");
+    costmap_2d::cuda::static_layer::rollingUpdateCosts(master_grid, transform, this, use_maximum_, min_i, min_j, max_i, max_j);
+    //costmap_2d::cuda::static_layer::rollingUpdateCosts(master_grid, transform, layered_costmap_->getCostmap(), use_maximum_, min_i, min_j, max_i, max_j);
     // Copy map data given proper transformations
     /*for (unsigned int i = min_i; i < max_i; ++i)
     {
