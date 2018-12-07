10d5a3b5f1b5147a9e92e732a4a1fc3781d8fbdd
Miau Shiauwe <miau.shiauwe@smail.nju.edu.cn>
2018-12-07 16:45:01 +0800

    Filled function body of two kernel functions

--- src/costmap_2d/plugins/inflation_layer.cu
+++ src/costmap_2d/plugins/inflation_layer.cu
@@ -1,21 +1,71 @@
 #include <costmap_2d/cuda_inflation_layer.h>
+#include <cmath>
 #include <cstring>
 #include <vector>
 
 #define TPB 512
 
+#define NO_INFORMATION 255
+#define INSCRIBED_INFLATED_OBSTACLE 253
+#define FREE_SPACE 0
+
+using std::abs;
 using std::memcpy;
 using std::vector;
 using costmap_2d::CellData;
 
 __global__ void setCostFloodingInflateUnkown(unsigned char *master, unsigned long master_size_x, unsigned long master_size_y, unsigned char *cachedCost_1D, unsigned int cacheSize, CellData *obstaclesArray, unsigned int obstaclesArray_count, unsigned int inflation_radius)
 {
+    int id=blockIdx.x*blockDim.x+threadIdx.x;
+    int temp=id;
 
+    int obstacleNum=temp/(2*inflation_radius+1)*(2*inflation_radius+1);
+    if(obstacleNum>obstaclesArray_count)
+        return;
+    
+    temp%=(2*inflation_radius+1)*(2*inflation_radius+1);
+    //Order of i and j are actually not so important, as it is with a square area
+    int deltax=temp/(2*inflation_radius+1)-inflation_radius;
+    int deltay=temp%(2*inflation_radius+1)-inflation_radius;
+
+    //Only obstacleNum may out of bounds. No need to check i and j again.
+    int x=obstaclesArray[obstacleNum].src_x_+deltax;
+    int y=obstaclesArray[obstacleNum].src_y_+deltay;
+    unsigned index=y*master_size_x+x;
+    unsigned char cost=cachedCost_1D[abs(deltax)*cacheSize+abs(deltay)];
+    unsigned char old_cost=master[index];
+
+    if(old_cost==NO_INFORMATION&&cost>FREE_SPACE)
+        master[index]=cost;
+    else
+        master[index]=cost>old_cost?cost:old_cost;
 }
 
 __global__ void setCostFloodingNoInflateUnkown(unsigned char *master, unsigned long master_size_x, unsigned long master_size_y, unsigned char *cachedCost_1D, unsigned int cacheSize, CellData *obstaclesArray, unsigned int obstaclesArray_count, unsigned int inflation_radius)
 {
+    int id=blockIdx.x*blockDim.x+threadIdx.x;
+    int temp=id;
+
+    int obstacleNum=temp/(2*inflation_radius+1)*(2*inflation_radius+1);
+    if(obstacleNum>obstaclesArray_count)
+        return;
     
+    temp%=(2*inflation_radius+1)*(2*inflation_radius+1);
+    //Order of i and j are actually not so important, as it is with a square area
+    int deltax=temp/(2*inflation_radius+1)-inflation_radius;
+    int deltay=temp%(2*inflation_radius+1)-inflation_radius;
+
+    //Only obstacleNum may out of bounds. No need to check i and j again.
+    int x=obstaclesArray[obstacleNum].src_x_+deltax;
+    int y=obstaclesArray[obstacleNum].src_y_+deltay;
+    unsigned index=y*master_size_x+x;
+    unsigned char cost=cachedCost_1D[abs(deltax)*cacheSize+abs(deltay)];
+    unsigned char old_cost=master[index];
+
+    if(old_cost==NO_INFORMATION&&cost>FREE_SPACE)
+        master[index]=cost;
+    else
+        master[index]=cost>old_cost?cost:old_cost;
 }
 
 void costmap_2d::cuda::inflation_layer::setCostFlooding(unsigned char *master, unsigned long master_size_x, unsigned long master_size_y, unsigned char **cached_cost, const vector<CellData> &obstacles, unsigned int inflation_radius, bool inflate_unknown)
